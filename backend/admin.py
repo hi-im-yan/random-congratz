d6e0d8ee32c40d770896e017947cc14d3375a2b2
yan ajiki <yanajiki2010@hotmail.com>
2021-05-10 21:19:09 -0400

    [Feito] Criação das models Categoria e Mensagem

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class Categoria_Admin(admin.ModelAdmin):
+    list_display = ('id', 'nome')
+
+class Message_Admin(admin.ModelAdmin):
+    list_display = ('id', 'message', 'categoria')
+
+    
+admin.site.register(Categoria, Categoria_Admin)
+admin.site.register(Message, Message_Admin)
--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=32)
+
+class Message(models.Model):
+    message = models.TextField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
