fb4ab5d027153bcb71bd24182dd8cf63481db970
yan ajiki <yanajiki2010@hotmail.com>
2021-05-10 21:30:44 -0400

    [Feito] Adicionado serializers e rotas buscando todo o banco

--- backend/api.py
+++ backend/api.py
@@ -3,8 +3,25 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+from .serializers import *
+from .models import *
+
 # Create your views here.
 
 @api_view(['GET'])
 def test_connection(request):
     return Response({"status": 200, "message": "OK", "description": "Connection to API is OK"})
+
+@api_view(['GET'])
+def get_categorias(request):
+    categorias = Categoria.objects.all()
+    serializer = CategoriaSerializer(instance=categorias, many=True)
+
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_messages(request):
+    messages = Message.objects.all()
+    serializer = MessageSerializer(instance=messages, many=True)
+
+    return Response(serializer.data)
--- backend/endpoints.py
+++ backend/endpoints.py
@@ -4,4 +4,6 @@ from . import api
 
 urlpatterns = [
     path('', api.test_connection, name='routes'),
+    path('categorias', api.get_categorias, name='categoria'),
+    path('mensagens', api.get_messages, name='mensagens'),
 ]
